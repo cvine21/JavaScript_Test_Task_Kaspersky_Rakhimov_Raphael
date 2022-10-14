edb46ee697996f6b1d1a223d390e609a9490ca30
cvine <rafarakh@gmail.com>
2022-10-14 12:32:52 +0300

    feat: hero page created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 import "./Home.scss";
 
 function Home() {
-	return <div className="content"></div>;
+	return (
+		<main className="hero">
+			<div className="center">
+				<img width={240} src="avataaars.svg" alt="avatar" />
+				<h1>USERS APP</h1>
+				<p>An application that displays a list of users.</p>
+				<Link to={"/users"}>
+					<button className="btn btn-lg btn-secondary fw-bold bg-dark">
+						Show list
+					</button>
+				</Link>
+			</div>
+		</main>
+	);
 }
 
 export default Home;
